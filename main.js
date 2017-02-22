65fcb33c89d2ef8f5c49713300d942607edbe4c9
Frisco Miranda <friscomiranda@gmail.com>
2017-02-21 19:22:05 -0800

    capitalize title and add home button that fades in and out as you scroll

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+//initialize parallax
 $(document).ready(function(){
   $('.parallax').parallax();
 });
 
+//this handles the scroll when you click on a menu item
 $(document).ready(function(){
   $("a").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
@@ -16,6 +18,22 @@ $(document).ready(function(){
       }, 800, function(){
         window.location.hash = hash;
       });
+    }
+  });
+});
+
+//this hides and reveals the back to top button on desktop
+$(document).ready(function() {
+  //this is the height of the screen they are using, and will be a different value depending on their window size
+  let windowHeight = window.innerHeight;
+  var duration = 1000;
+
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > windowHeight) {
+      $('.homeButton').fadeIn(duration);
+    } else {
+      $('.homeButton').fadeOut(duration);
     }
   });
 });
